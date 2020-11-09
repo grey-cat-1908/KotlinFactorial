d8e77cfe85964caebd9525c665b6331e940d38f6
grey-cat-1908 <grey.cat1908@gmail.com>
2020-11-09 18:05:34 +0300

    Удалил свои комментарии 2

--- src/Factorial.kt
+++ src/Factorial.kt
@@ -3,7 +3,6 @@ import javax.swing.JOptionPane.showMessageDialog
 import javax.swing.JOptionPane.PLAIN_MESSAGE
 
 fun factorial(n: Int): Double = if (n < 2) 1.0 else n * factorial(n - 1)
-
 val res =  showInputDialog(null,
         "Введите число",
         "Факториал", PLAIN_MESSAGE);
