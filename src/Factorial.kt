2918b736a015fc8aac7f1bedd56d51d19b7cd8c1
GreyCat1908 <61203964+grey-cat-1908@users.noreply.github.com>
2020-11-09 17:46:46 +0300

    Релиз

---
+++ src/Factorial.kt
@@ -0,0 +1,20 @@
+import javax.swing.JOptionPane.showInputDialog
+import javax.swing.JOptionPane.showMessageDialog
+import javax.swing.JOptionPane.PLAIN_MESSAGE
+
+fun factorial(n: Int): Double = if (n < 2) 1.0 else n * factorial(n - 1)
+
+//val number = Integer.valueOf(readLine())
+
+val res =  showInputDialog(null,
+        "Введите число",
+        "Факториал", PLAIN_MESSAGE);
+
+fun main() {
+    if (res == null) {
+        System.exit(0);
+    }
+    var number = Integer.parseInt(res);
+    var a = factorial(number);
+    showMessageDialog(null, "Результат: " + Math.round(a), "Факториал", PLAIN_MESSAGE);
+}
